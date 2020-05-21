3280932423ec4039304a073739d5ae0734746fa5
Maria Feudo <mfeudo@ucsc.edu>
2020-05-21 16:34:50 -0700

    another bug fix:(

--- src/scenes/EndingScene.js
+++ src/scenes/EndingScene.js
@@ -27,12 +27,15 @@ class EndingScene extends Phaser.Scene {
         }
         
         this.add.text(centerX, centerY - 300, 'Final Game Ending Scene', menuConfig).setOrigin(0.5);
-      
+        var monsterText = this.add.text(100, 150, 'Inner Self:', { fontFamily: 'Arial', fontSize: 32, color: '#00ff00' });
+        var monsterDialogue = this.add.text(200, 200, 'You\'ve come a long way', { fontFamily: 'Arial', fontSize: 32, color: '#00ff00' });
+        var continueText = this.add.text(550, 500, 'Press D to continue', { fontFamily: 'Arial', fontSize: 32, color: '#00ff00' });
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
         cursors = this.input.keyboard.createCursorKeys(); //arrow keys are now assigned and can be used
-       
+        keySpaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
 
 
         // reserving these keys for future interactions we implement
@@ -41,6 +44,8 @@ class EndingScene extends Phaser.Scene {
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
+        this.monsterArray = ([]);//array for monster
+
 
     
        
@@ -48,21 +53,27 @@ class EndingScene extends Phaser.Scene {
 
     update() {
 
-        if (Phaser.Input.Keyboard.JustDown(keyR) && (this.endMenuFlag)) {
+        if (Phaser.Input.Keyboard.JustDown(keyW)) {
 
             this.scene.start('worldScene');
 
         }
 
-        if (Phaser.Input.Keyboard.JustDown(keyD)) {
-           }
+        if (Phaser.Input.Keyboard.JustDown(keyA)) {
+           
+        
+        }
 
         if (cursors.left.isDown) // if the left arrow key is cave scene
         {
             this.scene.start('caveScene');
         }
         
-
+        
+        if (Phaser.Input.Keyboard.JustDown(keySpaceBar)) {
+           
+        this.monsterDialogue.setText('poop');
+        }
 
     }
 
