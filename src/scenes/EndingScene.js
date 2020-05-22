948a318559e004dc120fa50aa2ed63e8a4ecbd48
Maria Feudo <mfeudo@ucsc.edu>
2020-05-22 00:52:01 -0700

    initial

--- src/scenes/EndingScene.js
+++ src/scenes/EndingScene.js
@@ -27,12 +27,15 @@ class EndingScene extends Phaser.Scene {
         }
         
         this.add.text(centerX, centerY - 300, 'Final Game Ending Scene', menuConfig).setOrigin(0.5);
-      
+        var monsterText = this.add.text(100, 150, 'Inner Self:', { fontFamily: 'Arial', fontSize: 32, color: '#00ff00' });
+        this.monsterDialogue = this.add.text(200, 200, 'You\'ve come a long way', { fontFamily: 'Arial', fontSize: 32, color: '#00ff00' });
+        var continueText = this.add.text(550, 500, 'Press D to continue', { fontFamily: 'Arial', fontSize: 32, color: '#00ff00' });
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
         cursors = this.input.keyboard.createCursorKeys(); //arrow keys are now assigned and can be used
        
+
 
 
         // reserving these keys for future interactions we implement
@@ -41,29 +44,38 @@ class EndingScene extends Phaser.Scene {
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
+        this.monsterArray = ([]);//array for monster
+      
 
+        
+        this.monsterNumber = 0;
     
        
     }
 
     update() {
+        this.monsterDialogue.text = this.monsterArray[this.monsterNumber];
 
-        if (Phaser.Input.Keyboard.JustDown(keyR) && (this.endMenuFlag)) {
+        if (Phaser.Input.Keyboard.JustDown(keyW)) {
 
             this.scene.start('worldScene');
 
         }
 
-        if (Phaser.Input.Keyboard.JustDown(keyD)) {
-           }
+        if (Phaser.Input.Keyboard.JustDown(keyA)) {
+            if(this.monsterNumber<this.monsterArray.length){
+                this.monsterNumber++;
 
-        if (cursors.left.isDown) // if the left arrow key is cave scene
-        {
-            this.scene.start('caveScene');
-        }
+            }
+            this.groundClock = this.time.delayedCall(2500, () => { //delay call to spawn extra ground
+            }, null, this);
         
+        }
 
-
+       
+        
+        
+        
     }
 
 }
