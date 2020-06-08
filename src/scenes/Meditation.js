7a7f2db4c64df237678fa4ce2caff089b65ae716
Brian <brpereir@ucsc.edu>
2020-06-08 01:37:22 -0700

    meditation scene updates

--- src/scenes/Meditation.js
+++ src/scenes/Meditation.js
@@ -6,6 +6,8 @@ class Meditation extends Phaser.Scene {
     preload(){
         this.load.image('Meditation', './assets/Meditation.png');
         this.load.image('MeditationTree', './assets/MeditationTree.png');
+        this.load.image('ShadowSelf', './assets/ShadowLeftSmallSprite.png');
+        this.load.spritesheet('characterWalk', './assets/characterWalking.png', { frameWidth: 50, frameHeight: 150, startFrame: 0, endFrame: 31 })
         this.load.tilemapTiledJSON('meditationMap','./assets/TiledMeditation.json');
     }
 
@@ -23,8 +25,16 @@ class Meditation extends Phaser.Scene {
 
 
         this.player = new Player(this, centerX - 450, centerY - 40, 'characterWalk',0).setScale(0.4);
+        this.shadowSelf = this.add.sprite(centerX + 100, centerY + 50, 'ShadowSelf').setScale(0.4);
+        this.detectionArea = this.physics.add.sprite(centerX + 100, centerY + 50, 'TempSpoon');
+        this.detectionArea.alpha = 0;
         this.tree = this.add.tileSprite(0, 0, 960, 640, 'MeditationTree').setOrigin(0,0);
         this.physics.add.collider(this.player, backgroundLayer);
+
+        this.physics.add.collider(this.detectionArea, this.player, (a, b) => {
+            this.scene.start('endingSce');
+        }, null, this);
+
 
          // this allows us to quickly use up, left, down, right arroy keys
          cursors = this.input.keyboard.createCursorKeys();
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -125,7 +125,7 @@ class Menu extends Phaser.Scene {
 
         if (cursors.up.isDown) // up arrow key takes you to meditation scene
         {
-            this.scene.start('creditScene');
+            this.scene.start('meditationScene');
         }
         
 
